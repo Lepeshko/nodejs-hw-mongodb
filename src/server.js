439de80780ec449c2cc7688ee471b9202f5b20fa
Lepeshko <anatolii.lepeshko1@gmail.com>
2024-10-02 15:21:00 +0300

    commit_02.10.24_15.21

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+export const startServer = () => {
+  const app = express();
+
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  });
+
+  app.use(logger);
+  app.use(cors());
+  app.use(express.json());
+
+  // routes
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: `${req.url} not found`,
+    });
+  });
+
+  app.use((error, req, res, next) => {
+    res.status(500).json({
+      message: error.message,
+    });
+  });
+
+  app.listen(3000, () => console.log('Server running on port 3000'));
+};
