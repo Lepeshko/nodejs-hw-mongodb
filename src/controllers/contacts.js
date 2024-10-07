3b452212f271f76480cc579d815dde1ec2c4b81b
Lepeshko <anatolii.lepeshko1@gmail.com>
2024-10-07 09:18:41 +0300

    commit_07.10.24_09.19

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -68,7 +68,7 @@ export const patchContactController = async (req, res) => {
   res.json({
     status: 200,
     message: 'Successfully patched a contact!',
-    data: result,
+    data: result.data,
   });
 };
 
